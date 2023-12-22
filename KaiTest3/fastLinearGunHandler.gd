c7a4d00ffb64cf0690934bbcc362033bdc153db3
qwertychomp <supersequoia11@gmail.com>
2023-12-22 11:50:43 -0800

    harder better faster bullet bullet

---
+++ KaiTest3/fastLinearGunHandler.gd
@@ -0,0 +1,36 @@
+extends Node2D
+@onready var masterBullet = get_node("masterBullet")
+@onready var timePerShotIncrementer:float
+@onready var shotsFired:int
+@onready var reloadTimeCounter:float
+@onready var currentScene
+#all time based gun metadata in seconds
+func fireBullet():
+	print("ass")
+	var bulletClone = masterBullet.duplicate(15)
+	bulletClone.visible = true
+	currentScene.add_child(bulletClone)
+	bulletClone.position = self.position
+	bulletClone.fire(self.rotation,get_meta("Damage"),get_meta("bulletSpeed"))
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	currentScene = get_tree().get_current_scene()
+func _physics_process(delta):
+	print("ahhhhhhhh godot code make me insane")
+	print(timePerShotIncrementer)
+	print(timePerShotIncrementer>=self.get_meta("shotSpeed"))
+	timePerShotIncrementer += delta/2
+	if(shotsFired>=self.get_meta("clipSize")):
+		print("shit")
+		if(shotsFired==self.get_meta("clipSize")):
+			reloadTimeCounter = 0
+			shotsFired += 1
+		reloadTimeCounter += delta
+		if(reloadTimeCounter<self.get_meta("reloadTime")):
+			return
+		shotsFired = 0
+	elif(timePerShotIncrementer>=self.get_meta("shotSpeed")):
+		for x in self.get_meta("shotSpeed")/timePerShotIncrementer:
+			fireBullet()
+		shotsFired += 1
+		timePerShotIncrementer = 0
