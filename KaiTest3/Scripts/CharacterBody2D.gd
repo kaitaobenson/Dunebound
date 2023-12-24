b886603dca0008fab75c653a059f66d7c5c9aeca
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2023-12-23 19:04:12 -0800

    Clean up and fix animation

--- KaiTest3/Scripts/CharacterBody2D.gd
+++ KaiTest3/Scripts/CharacterBody2D.gd
@@ -13,6 +13,8 @@ var health:int = 100;
 
 func ready():
 	pass;
+	
+# WALK
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -22,9 +24,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#its my keybind systems time to shine!
 	var direction = Input.get_axis("ui_left", "move-right")
 	
 	#horiz movement
@@ -32,30 +31,30 @@ func _physics_process(delta):
 		anim.stop()
 	
 	if direction == 1:
-		anim.play("Run")
+		anim.play("Walk")
 		animSprite.flip_h = false
 		
 		velocity.x = direction * SPEED
 	if direction == -1:
-		anim.play("Run")
+		anim.play("Walk")
 		animSprite.flip_h = true
 		
 		velocity.x = direction * SPEED
 	if direction == 0:
 		velocity.x = 0
 
-	if velocity.y > 0:
-		pass;
-
 	move_and_slide()
+
 func _hitByBullet(damage:int):
 	print("hit by bullet")
 	doDamage(damage)
+	
 func doDamage(damage:int):
 	health = health - damage
 	print(health)
 	if(health<0):
 		die()
+
 func die():
 	self.queue_free()
 	#whatever you wanna do when you die
