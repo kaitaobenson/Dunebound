59edc96dd003f703a37c590240fcbf9a747b15cc
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-01-22 13:39:26 -0800

    add run metadata

--- KaiTest3/Scripts/CharacterBody2D.gd
+++ KaiTest3/Scripts/CharacterBody2D.gd
@@ -55,17 +55,18 @@ func grapple(delta):
 
 func movement(delta):
 	
-	const SPEED = 300.0
-	const JUMP_VELOCITY = -400.0
-	const gravity = 500;
+	var SPEED = get_meta("SPEED")
+	var JUMP_VELOCITY = get_meta("JUMP")
+	var gravity = get_meta("GRAVITY");
 	var direction = Input.get_axis("ui_left", "move-right")
 	
 	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		print(gravity)
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -JUMP_VELOCITY
 	# Horiz movement
 	if velocity.x == 0:
 		anim.stop()
