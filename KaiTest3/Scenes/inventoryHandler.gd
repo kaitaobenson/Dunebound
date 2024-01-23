c2e0c500b353688e560da7f1bc40044655e6293c
qwertychomp <supersequoia11@gmail.com>
2024-01-23 09:13:44 -0800

    Revert "Revert "ivnentory window go crazy""
    This reverts commit 1758b6eadf01ec442d7ceb3f4c9fb01289728b15.

---
+++ KaiTest3/Scenes/inventoryHandler.gd
@@ -0,0 +1,16 @@
+extends Node2D
+var closeButton:Button
+func _ready():
+	closeButton = get_node("closeButton")
+	closeButton.connect("pressed",invClose)
+	closeButton.disabled = false
+func invClose():
+	self.visible = false
+	closeButton.disabled = true
+func invToggle():
+	self.visible = !self.visible
+	closeButton.disabled = !closeButton.disabled
+	print(closeButton.disabled)
+func _process(delta):
+	if(Input.is_action_just_pressed("inventory_toggle")):
+		invToggle()
