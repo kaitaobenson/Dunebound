55e2481f2a587f538171ee8fd0acb87ba961ec51
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-01-22 19:56:47 -0800

    add art folder

--- KaiTest3/Scripts/CharacterBody2D.gd
+++ KaiTest3/Scripts/CharacterBody2D.gd
@@ -63,7 +63,6 @@ func movement(delta):
 	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		print(gravity)
 	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
