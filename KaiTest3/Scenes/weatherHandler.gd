1c5f6a71cf6bdba6c831f27338adbef573350a4f
qwertychomp <supersequoia11@gmail.com>
2024-02-23 12:05:39 -0800

    fix the cycler.gd thing

---
+++ KaiTest3/Scenes/weatherHandler.gd
@@ -0,0 +1,34 @@
+extends Node2D
+#Daylength in Seconds
+
+const DAY_LENGTH:float = 10
+const PHASE_LENGTH:float = DAY_LENGTH / 6.0
+const BEGIN_PHASE:float = 0
+
+var elapsedTime:float = 0.0
+var totalElapsedTime:float = 0.0
+
+#Gradient for Night / Day
+var gradientResource = load("res://Assets/Textures/DayNightGradient.tres")
+
+func _process(delta):
+	elapsedTime += delta
+	totalElapsedTime += delta
+	day_night_visuals()
+	if elapsedTime >= PHASE_LENGTH:
+		new_day_phase()
+
+func new_day_phase():
+	elapsedTime = 0
+	#print(Global.TimeOfDay)
+	if Global.TimeOfDay < 6:
+		Global.TimeOfDay += 1
+	else:
+		Global.TimeOfDay = 1
+		Global.DayCount += 1
+		
+func day_night_visuals():
+	var colorValue:float = (sin(PI / DAY_LENGTH * (totalElapsedTime + DAY_LENGTH * BEGIN_PHASE - 0.5)) + 1) / 2
+	#print (colorValue)
+	$"../CanvasModulate".set_color(gradientResource.sample(colorValue))
+	$"../Background/CanvasModulate".set_color(gradientResource.sample(colorValue))
