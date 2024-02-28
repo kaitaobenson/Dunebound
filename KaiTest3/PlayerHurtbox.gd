77eb63a849b8ba7828542dbbb2a603b86a62f91c
Schoondaddy <Xavierbunbunny@gmail.com>
2024-02-27 20:22:13 -0800

    made the ray of casters looonger

--- KaiTest3/PlayerHurtbox.gd
+++ KaiTest3/PlayerHurtbox.gd
@@ -18,20 +18,19 @@ func _process(delta):
 	#print(bodies_in_hitbox)
 	check_for_enemies(delta)
 	
-	
 func check_for_enemies(delta):
 	if _i_frames_done:
 		if bodies_in_hitbox.has("jimmy"):
 			var attack = Attack.new()
 			attack.attack_damage = 20
 			health.damage(attack)
-			i_frames_on(1)
+			i_frames_on(1.0)
 			
 		elif bodies_in_hitbox.has("Zombie Husk Guy"):
 			var attack = Attack.new()
 			attack.attack_damage = 30
 			health.damage(attack)
-			i_frames_on(1)
+			i_frames_on(1.0)
 			
 			
 func i_frames_on(seconds : float):
