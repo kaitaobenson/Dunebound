d79cb24cd5b8ca6f626508edc7d89001b3abea08
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-03-01 19:52:26 -0800

    animation code updates, jump still looks goofy

--- KaiTest3/PlayerAnimations.gd
+++ KaiTest3/PlayerAnimations.gd
@@ -12,7 +12,7 @@ enum ALL_ANIMATIONS {
 	}
 
 var current_animations = [ALL_ANIMATIONS.IDLE]
-var playing_animation : ALL_ANIMATIONS
+var playing_animation : ALL_ANIMATIONS = ALL_ANIMATIONS.NONE
 
 var jump = 0
 
@@ -20,20 +20,7 @@ var jump = 0
 @onready var _anim_sprite = $AnimatedSprite2D
 @onready var _player = $"../"
 
-func _process(delta):
-	if current_animations.has(ALL_ANIMATIONS.ATTACK):
-		play_animation(ALL_ANIMATIONS.ATTACK)
-		
-	elif current_animations.has(ALL_ANIMATIONS.JUMP):
-		play_animation(ALL_ANIMATIONS.JUMP)
-		
-	elif current_animations.has(ALL_ANIMATIONS.RUN):
-		play_animation(ALL_ANIMATIONS.RUN)
-		
-	elif current_animations.has(ALL_ANIMATIONS.IDLE):
-		play_animation(ALL_ANIMATIONS.IDLE)
-		
-		
+
 func change_animation(name:ALL_ANIMATIONS, isOn:bool):
 	if isOn:
 		if current_animations.has(name) == false:
@@ -43,55 +30,60 @@ func change_animation(name:ALL_ANIMATIONS, isOn:bool):
 			current_animations.erase(name)
 			
 			
+			
+func _process(delta):
+	if current_animations.has(ALL_ANIMATIONS.ATTACK):
+		if playing_animation != ALL_ANIMATIONS.ATTACK:
+			play_animation(ALL_ANIMATIONS.ATTACK)
+			print("ATTACK")
+		
+	elif current_animations.has(ALL_ANIMATIONS.JUMP):
+		if playing_animation != ALL_ANIMATIONS.JUMP:
+			print("JUMP")
+			play_animation(ALL_ANIMATIONS.JUMP)
+		
+	elif current_animations.has(ALL_ANIMATIONS.RUN):
+		if playing_animation != ALL_ANIMATIONS.RUN:
+			play_animation(ALL_ANIMATIONS.RUN)
+			print("RUN")
+			
+	elif current_animations.has(ALL_ANIMATIONS.IDLE):
+		if playing_animation != ALL_ANIMATIONS.IDLE:
+			play_animation(ALL_ANIMATIONS.IDLE)
+			print("IDLE")
+			
+			
+			
 func play_animation(animationName:ALL_ANIMATIONS):
-	#RUN
-	if animationName == ALL_ANIMATIONS.RUN && playing_animation != ALL_ANIMATIONS.RUN:
-		stop_animation_if_not_playing(ALL_ANIMATIONS.RUN)
+	if animationName == ALL_ANIMATIONS.RUN:
 		_anim.play("Run")
 		playing_animation = ALL_ANIMATIONS.RUN
 		
-	#ATTACK
-	if animationName == ALL_ANIMATIONS.ATTACK && playing_animation != ALL_ANIMATIONS.ATTACK:
-		stop_animation_if_not_playing(ALL_ANIMATIONS.ATTACK)
+		
+	if animationName == ALL_ANIMATIONS.ATTACK:
 		_anim.play("Slash1")
 		playing_animation = ALL_ANIMATIONS.ATTACK
 		
 		await _anim.animation_finished
 		current_animations.erase(ALL_ANIMATIONS.ATTACK)
 		
-	#IDLE
-	if animationName == ALL_ANIMATIONS.IDLE && playing_animation != ALL_ANIMATIONS.IDLE:
-		stop_animation_if_not_playing(ALL_ANIMATIONS.IDLE)
+		
+	if animationName == ALL_ANIMATIONS.IDLE:
 		_anim.play("Idle")
 		playing_animation = ALL_ANIMATIONS.IDLE
 		
-	#JUMP
-	if animationName == ALL_ANIMATIONS.JUMP:
-		if jump == 0:
-			stop_animation_if_not_playing(ALL_ANIMATIONS.JUMP_UP)
-			playing_animation = ALL_ANIMATIONS.JUMP
-			_anim.play("JumpUp")
-			if _player.velocity.y > 0:
-				jump = 1
-				
-		if jump == 1:
-			stop_animation_if_not_playing(ALL_ANIMATIONS.JUMP_DOWN)
-			_anim.play("JumpDown")
-			if _player.is_on_floor():
-				jump = 2
-				
-		if jump == 2:
-			stop_animation_if_not_playing(ALL_ANIMATIONS.JUMP_LAND)
-			_anim.play("JumpLand")
-			await get_tree().create_timer(0.1).timeout
-			jump = 3
-			
-		if jump == 3:
-			current_animations.erase(ALL_ANIMATIONS.JUMP)
-			jump = 0 
-			
-			
-func stop_animation_if_not_playing(animation : ALL_ANIMATIONS):
-	if playing_animation != animation:
-		_anim.stop()
 		
+	if animationName == ALL_ANIMATIONS.JUMP:
+		playing_animation = ALL_ANIMATIONS.JUMP
+		_anim.play("JumpUp")
+		
+		while _player.velocity.y <= 0:
+			await get_tree().create_timer(0.1).timeout
+		_anim.play("JumpDown")
+		
+		while _player.is_on_floor() == false:
+			await get_tree().create_timer(0.1).timeout
+		_anim.play("JumpLand")
+		
+		await _anim.animation_finished
+		current_animations.erase(ALL_ANIMATIONS.JUMP)
