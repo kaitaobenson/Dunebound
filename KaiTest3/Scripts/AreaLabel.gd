4401ea94e1b1c93c5762e8a469cb228b0e12ee61
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-04-03 20:09:44 -0700

    fix dependencies and area names

--- KaiTest3/Scripts/AreaLabel.gd
+++ KaiTest3/Scripts/AreaLabel.gd
@@ -2,22 +2,55 @@ extends Node2D
 
 var player_is_inside_area: bool = false
 var has_shown_label: bool = false
+var can_show: bool = true
+
+var fade_in
+var fade_out
+
+@onready var timer = get_tree().create_timer(5)
 
 @onready var text = $"CanvasLayer/RichTextLabel"
 
 func _ready():
+	fade_in = get_tree().create_tween()
+	fade_out = get_tree().create_tween()
+	
 	text.modulate.a = 0
 
 func _process(delta):
-	if (player_is_inside_area) && (!has_shown_label):
-		print("1")
-		has_shown_label = true
-		await get_tree().create_tween().tween_property(text, "modulate:a", 1, 9)
-		
+	if has_shown_label && timer.time_left == 0:
+		do_fade_out()
+	if !has_shown_label:
+		timer = get_tree().create_timer(5)
+	if !has_shown_label == true && can_show == false:
+		if !player_is_inside_area:
+			can_show = true
+	
+	if (player_is_inside_area) && (!has_shown_label) && (can_show):
+		print("fadingin")
+		do_fade_in()
 	if (!player_is_inside_area) && (has_shown_label):
-		print("2")
-		has_shown_label = false
-		await get_tree().create_tween().tween_property(text, "modulate:a", 0, 4)
+		can_show = true
+		print("fadingout")
+		do_fade_out()
+
+
+func do_fade_in():
+	can_show = false
+	has_shown_label = true
+	fade_out.stop()
+	
+	fade_in = get_tree().create_tween()
+	fade_in.tween_property(text, "modulate:a", 1, 5)
+
+func do_fade_out():
+	has_shown_label = false
+	#print("faing outt!!!!")
+	fade_in.stop()
+	
+	fade_out = get_tree().create_tween()
+	fade_out.tween_property(text, "modulate:a", 0, 2)
+
 
 func _on_area_2d_body_entered(body):
 	if body == Global.Player:
