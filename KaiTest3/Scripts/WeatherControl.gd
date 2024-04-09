61e1344ec8f46207ae12b0ceb4e9a411a8a4d6fc
Schoondaddy <Xavierbunbunny@gmail.com>
2024-04-08 22:11:34 -0700

    fix kai equation, many a slide bug to be fixed

--- KaiTest3/Scripts/WeatherControl.gd
+++ KaiTest3/Scripts/WeatherControl.gd
@@ -1,9 +1,11 @@
 extends Node
 #Daylength in Seconds
 
+const NUMBER_OF_PHASES : float = 6.0
 const DAY_LENGTH:float = 10
-const PHASE_LENGTH:float = DAY_LENGTH / 6.0
+const PHASE_LENGTH:float = DAY_LENGTH / NUMBER_OF_PHASES
 const BEGIN_PHASE:float = 0
+
 
 var elapsedTime:float = 0.0
 var totalElapsedTime:float = 0.0
@@ -31,19 +33,20 @@ func _process(delta):
 
 func new_day_phase():
 	elapsedTime = 0
-	if Global.TimeOfDay < 6:
+	if Global.TimeOfDay < NUMBER_OF_PHASES:
 		Global.TimeOfDay += 1
 	else:
 		Global.TimeOfDay = 1
 		Global.DayCount += 1
 		
 func day_night_visuals():
-	var colorValue:float = (sin(PI / DAY_LENGTH * (totalElapsedTime + DAY_LENGTH * BEGIN_PHASE - 0.5)) + 1) / 2
+	var colorValue:float = (sin(2 * PI / DAY_LENGTH * (totalElapsedTime - DAY_LENGTH * (0.25 + BEGIN_PHASE / NUMBER_OF_PHASES) - 1)) + 1) / 2
 	$"../CanvasModulate".set_color(gradientResource.sample(colorValue))
 	$"../BackgroundContainer/ParallaxBackground/CanvasModulate".set_color(gradientResource.sample(colorValue))
+	print(Global.TimeOfDay)
 	
 func update_temperature():
-	var heatValue:float = (sin(PI / DAY_LENGTH * (totalElapsedTime + DAY_LENGTH * BEGIN_PHASE - 0.5)) + 1) / 2
+	var heatValue:float = (sin(2 * PI / DAY_LENGTH * (totalElapsedTime - DAY_LENGTH * (0.25 + BEGIN_PHASE / NUMBER_OF_PHASES) - 1)) + 1) / 2
 	Global.temperature = heatValue * 100
 
 func sandstorm():
