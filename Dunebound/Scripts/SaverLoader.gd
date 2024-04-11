1a3dfe196ebaadb11b1afd900125a47cb4768d38
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-04-11 08:54:18 -0700

    saver/loader now can be disabled

--- Dunebound/Scripts/SaverLoader.gd
+++ Dunebound/Scripts/SaverLoader.gd
@@ -1,23 +1,28 @@
 class_name SaveGame
 extends Node
 
+@export var turned_on: bool = true
+
 const save_path = "user://savegame.tres"
 var loaded
 var save_dict: Dictionary = {
 }
 
 var default_dict: Dictionary = {
-	"PlayerPos" = Vector2(0,0),
+	"PlayerPos" = Global.Player.global_position,
 	"Health" = 100
 }
+
 func _init():
 	loader()
 	Global.saver_loader = self
 
-
 func var_update(value, var_name):
 	save_dict[var_name] = value
-	save()
+	if turned_on:
+		save()
+	else:
+		clear_save()
 
 func save() -> void:
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
@@ -35,3 +40,7 @@ func find_saved_value(desired_var):
 		save_dict[desired_var] = default_dict[desired_var]
 	return save_dict[desired_var]
 
+func clear_save():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	save_dict.clear()
+	file.store_var(save_dict)
