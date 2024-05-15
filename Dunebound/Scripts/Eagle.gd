f4a03f4020a5098958ce6e08b5d44395dc6c90a8
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-05-15 07:40:52 -0700

    eagle 1

--- Dunebound/Scripts/Eagle.gd
+++ Dunebound/Scripts/Eagle.gd
@@ -2,10 +2,42 @@ extends Node2D
 
 
 @onready var begin_pos = position
+@onready var area = $"Area2D"
 
-var wander_speed = 10
-var wander_angle = 0
+
+var dist_from_begin_pos = 0
+var angle_to_begin_pos = 0
+
+var wander_speed = 2
+var wander_angle = 45
 
 
 func _physics_process(delta):
-	pass
+	dist_from_begin_pos = position.distance_to(begin_pos)
+	angle_to_begin_pos = rad_to_deg(position.angle_to(begin_pos))
+	
+	if area.get_overlapping_bodies().has(Global.Player):
+		# ATTACK
+		pass
+	else:
+		# WANDER
+		if dist_from_begin_pos > 100:
+			pass
+			# GO BACK
+			move_towards(wander_angle, wander_speed)
+			
+		else:
+			pass
+			# GO FORWARDS
+
+
+func move_towards(angle: int, speed: int):
+	var x_speed
+	var y_speed
+	
+	angle = ((angle % 360) + 360) % 360
+	x_speed = speed * cos(deg_to_rad(angle))
+	y_speed = speed * sin(deg_to_rad(angle))
+	
+	position.x += x_speed
+	position.y += y_speed
--- Dunebound/Scripts/PlayerScripts/Player.gd
+++ Dunebound/Scripts/PlayerScripts/Player.gd
@@ -212,7 +212,7 @@ func die():
 				if Global.current_scene_path == "res://Scenes/Levels/WORLD.tscn":
 					Global.root_node.change_level_to_scene("res://Scenes/Levels/WORLD.tscn")
 			elif Global.death_ui.is_quit_pressed():
-				Global.root_node.change_level_to_scene("res://Scenes/Levels/TitleScreen.wwwwwwwwtscn")
+				Global.root_node.change_level_to_scene("res://Scenes/Levels/TitleScreen.tscn")
 				
 			await get_tree().process_frame
 
