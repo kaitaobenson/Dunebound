6108e7c182f9113882a354314c92ddf9e4e4b93d
kaitaobenson <126978538+kaitaobenson@users.noreply.github.com>
2024-05-17 12:32:31 -0700

    completed world building

--- Dunebound/Scripts/InvisibleWalls.gd
+++ Dunebound/Scripts/InvisibleWalls.gd
@@ -1,22 +1,15 @@
 extends Node
 
 @export var area: Area2D
-@export var sprite: Node
+@export var tilemap: TileMap
+
+func _ready():
+	tilemap.tile_set.remove_physics_layer(0)
+	
 
 func _process(delta):
-	for x in range(sprite.get_used_rect().size.x):
-		for y in range(sprite.get_used_rect().size.y):
-			# Get the tile ID at this position
-			var tile_id = sprite.get_cell(x, y)
-			
-			# Check if the tile is valid and has collision
-			if tile_id != -1 and sprite.tile_set.tile_get_collision(tile_id) != 0:
-				# Disable collision for this tile
-				sprite.tile_set.tile_set_collision(tile_id, false)
-				
-	
 	if area.get_overlapping_bodies().has(Global.Player):
-		sprite.modulate.a = lerp(sprite.modulate.a, 0.0, 0.05)
+		tilemap.modulate.a = lerp(tilemap.modulate.a, 0.0, 0.05)
 		print("sdfsdf")
-	elif sprite.modulate.a != 1:
-		sprite.modulate.a = lerp(sprite.modulate.a, 1.0, 0.05)
+	elif tilemap.modulate.a != 1:
+		tilemap.modulate.a = lerp(tilemap.modulate.a, 1.0, 0.05)
