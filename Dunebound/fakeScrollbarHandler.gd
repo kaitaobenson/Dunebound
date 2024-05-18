ab55efdb5c87e9a1a47d34988c15584930a2b8d2
Sequoia Haynes <supersequoia11@gmail.com>
2024-05-18 08:09:48 -0700

    oops

--- Dunebound/fakeScrollbarHandler.gd
+++ Dunebound/fakeScrollbarHandler.gd
@@ -1,11 +1,13 @@
 extends ColorRect
-
-
+var min_value = 0
+var max_value = 100
+var value = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	get_node("scroll").size = Vector2(self.size.x,max_value/self.size.y)
+func _input(event):
+	if(event is InputEventMouseMotion):
+		pass
+		#finish this goofy stuff at convention
 func _process(delta):
 	pass
